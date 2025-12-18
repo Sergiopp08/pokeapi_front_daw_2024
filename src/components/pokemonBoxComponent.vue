3b1ae675545dcc69889814eb042be67e5b9ad77e
Sergiopp08 <sergiopperez08@gmail.com>
2025-12-18 18:26:32 +0100

    prueba 121212

--- src/components/pokemonBoxComponent.vue
+++ src/components/pokemonBoxComponent.vue
@@ -1,11 +1,5 @@
 <template>
-  <RouterLink class="pokemon-box-item" :to="to">
-    <div class="pokemon-box-img-container">
-      <img :src="img" :alt="`Imagen de ${name}`" />
-    </div>
 
-    <span>{{ number + " " + name }}</span>
-  </RouterLink>
 </template>
 
 <script setup>
